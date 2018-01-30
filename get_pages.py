29058b701bd2387b6bc67fb10d48d7a4b5a804c3
danagilliann <danalee96@gmail.com>
2018-01-29 23:08:03 -0500

    time parsed

--- get_pages.py
+++ get_pages.py
@@ -2,11 +2,15 @@ import facebook
 import requests
 import csv
 
+from datetime import datetime
 from secrets import secrets
 
 access_token = secrets["access_token"]
 user_id = secrets["user_id"]
 pages = []
+
+# previous_time = 0
+next_time = 100
 
 with open('pages.csv', 'r') as pagefile:
     pagereader = csv.reader(pagefile, delimiter=',')
@@ -18,5 +22,10 @@ graph = facebook.GraphAPI(access_token)
 
 for page in pages:
     posts = graph.get_connections(page, 'feed')
+    posts = posts["data"]
 
-    print(posts)
+    for post in posts:
+        time_posted = datetime.strptime(post["created_time"], '%Y-%m-%dT%H:%M:%S%z')
+        mili_time = time_posted.timestamp() * 1000
+
+        print(mili_time)
